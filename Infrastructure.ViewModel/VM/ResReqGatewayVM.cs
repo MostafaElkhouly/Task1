febe7bc80a0eb2c77ded8d5ebf6fab249fc3b0f6
Mostafa Anwar <Mostafa.anwar.gawad@gmail.com>
2021-12-13 01:36:27 +0200

    Fix Issue

--- Infrastructure.ViewModel/VM/ResReqGatewayVM.cs
+++ Infrastructure.ViewModel/VM/ResReqGatewayVM.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.ViewModel.VM
 {
-    public class Gateway 
+    public class ResReqGatewayVM 
     {
         public Guid Id { set; get; }
         public DateTime DateOfCreate { set; get; }
         public string SerialNumber { set; get; }
         public string Name { set; get; }
         public string IPv4 { set; get; }
-        public List<PeripheralDevice> PeripheralDevices { set; get; }
+        public List<ResReqPeripheralDeviceVM> PeripheralDevices { set; get; }
     }
 }
